640d64bc3ec2231648fde88587e78b46e11ac03b
rbogart1990 <robbyt543@gmail.com>
2024-07-17 11:36:13 -0400

    overwrite delta table with new data

--- create_delta_table.py
+++ create_delta_table.py
@@ -82,6 +82,32 @@ LOG.info("Reading and displaying the Delta table...")
 df = spark.read.format("delta").load("/tmp/delta-table")
 df.show()
 
+
+# Overwrite original Delta table with new data
+new_data = generate_data(start_id=6, end_id=10)
+
+# Create DataFrame
+df_new = spark.createDataFrame(new_data, schema=schema)
+
+# Path to save the Delta table
+delta_table_path = "/tmp/delta-table"
+
+# Remove existing Delta table if it exists
+if os.path.exists(delta_table_path):
+    LOG.info(f"Removing existing Delta table at {delta_table_path}...")
+    spark._jvm.org.apache.hadoop.fs.FileSystem.get(spark._jsc.hadoopConfiguration()).delete(
+        spark._jvm.org.apache.hadoop.fs.Path(delta_table_path), True
+    )
+
+# Save DataFrame as Delta table
+LOG.info("Saving DataFrame with new data as Delta table...")
+df_new.write.format("delta").mode("overwrite").save(delta_table_path)
+
+# Read the Delta table and show the records
+LOG.info("Reading and displaying the Delta table...")
+df = spark.read.format("delta").load("/tmp/delta-table")
+df.show()
+
 LOG.info("Getting history...")
 deltaTable = DeltaTable.forPath(spark, "/tmp/delta-table")
 history = deltaTable.history()
