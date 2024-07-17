ca6b5ecdb701e58e6ab4078219202f44e83657de
rbogart1990 <robbyt543@gmail.com>
2024-07-17 13:10:07 -0400

    call generate_data function from reuseable script

--- create_delta_table.py
+++ create_delta_table.py
@@ -1,33 +1,10 @@
-from datetime import datetime, timedelta
-from decimal import Decimal
 from delta import *
 import logging
 import os
 import pyspark
 from pyspark.sql.types import StructType, StructField, IntegerType, DateType, DecimalType
-import random
-from typing import List, Tuple
 
-def generate_data(start_id: int, end_id: int) -> List[Tuple[int, datetime.date, Decimal]]:
-    """
-    Generate sample data with IDs ranging from start_id to end_id.
-
-    Args:
-        start_id (int): The starting ID for the range.
-        end_id (int): The ending ID for the range.
-
-    Returns:
-        List[Tuple[int, datetime.date, Decimal]]: A list of tuples containing the generated data.
-            Each tuple consists of (id, transaction_date, amount).
-    """
-    today = datetime.now()
-    data = []
-    for i in range(start_id, end_id + 1):
-        id = i
-        transaction_date = today - timedelta(days=random.randint(0, 365))
-        amount = Decimal(round(random.uniform(10.0, 1000.0), 2))
-        data.append((id, transaction_date, amount))
-    return data
+from utils import generate_data
 
 # Set up logging
 logging.basicConfig(
@@ -59,9 +36,6 @@ delta_table_path = "/tmp/delta-table"
 LOG.info("Create Spark DataFrame and save as Delta Table")
 # Generate data
 LOG.info("Generating data...")
-today = datetime.now()
-data = []
-
 data = generate_data(start_id=1, end_id=5)
 
 # Create DataFrame
